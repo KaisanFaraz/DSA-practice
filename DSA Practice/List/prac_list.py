1a02f939c138585fe61db960445484c9bc6d957d
KaisanFaraz <livinglouder759@gmail.com>
2026-04-09 07:39:45 +0600

    Add files via upload

---
+++ DSA Practice/List/prac_list.py
@@ -0,0 +1,43 @@
+'''
+list1 = [1,"Apple",23.21, True,291]
+list1[0] = 20
+list1.append(100)
+list1.append([2,3,1])
+list1.extend([4,5,6])
+print(len(list1))
+print(list1[-2])
+print(list1[1])
+print(list1)  
+'''
+'''
+list2 = [1,2,3,5,6,7]
+list2.insert(3,4)
+print(list2)
+
+'''
+'''
+list3 = [1,2,3,4,5]
+list3.remove(2)    #specific number remove 
+list3.pop()           #specific index remove 
+list3.pop(0)
+list3.clear()
+print(list3)
+'''
+
+'''
+list4 = [1,2,3,4,5,6,7,8]
+print(min(list4))
+print(max(list4))
+'''
+'''
+list5 = [1,2,3,4,1,2,2,3,3,4,4,5,4,5,6,5]
+print(list5.count(4))
+list5.sort()
+print(list5)
+'''
+'''
+       # 0 1 2 3 4 5 6
+list6 = [1,2,3,4,5,6,7]
+print(list6[2:6])
+print(list6[2:6:2])
+'''
